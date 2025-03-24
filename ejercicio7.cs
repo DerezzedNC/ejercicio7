d022ecfcfce0ab4d7531f93624e1cfb16adaf3e0
Angel Noh <angelgabrielnohcamargo@gmail.com>
2025-03-24 17:17:51 -0600

    Add files via upload

---
+++ ejercicio7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string rutaArchivo = "texto.txt";
+
+  
+        Console.WriteLine("Escribe un texto:");
+        string texto = Console.ReadLine();
+
+      
+        File.WriteAllText(rutaArchivo, texto);
+        Console.WriteLine("\nTexto guardado en 'texto.txt'.");
+
+       
+        string textoLeido = File.ReadAllText(rutaArchivo);
+
+        
+        string[] palabras = textoLeido.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        int cantidadPalabras = palabras.Length;
+
+        Console.WriteLine($"\nEl texto contiene {cantidadPalabras} palabra(s).");
+    }
+}
